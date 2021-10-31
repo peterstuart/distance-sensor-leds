de9ec40e687ecc1fb25756a4303b878c260af0dd
Peter Stuart <peter@peterstuart.org>
2021-10-31 08:37:56 -0400

    Use timer with interrupt rather than delay (#4)

--- src/hardware.rs
+++ src/hardware.rs
@@ -3,30 +3,29 @@ use stm32f3_discovery::{
     leds::Leds,
     stm32f3xx_hal::{
         adc::{self, Adc},
-        delay::Delay,
         gpio::{Analog, Gpioe, Pin, U},
-        pac::{self, ADC3},
+        pac::{self, ADC3, TIM2},
         prelude::*,
+        time::rate::Hertz,
+        timer::Timer,
     },
 };
 
-pub struct Init {
-    pub delay: Delay,
+pub struct Hardware {
     pub leds: Leds,
     pub distance_sensor: Gp2d12<Pin<Gpioe, U<7_u8>, Analog>, Adc<ADC3>>,
+    pub timer: Timer<TIM2>,
 }
 
-impl Init {
+impl Hardware {
     pub fn take() -> Option<Self> {
         let mut peripherals = pac::Peripherals::take()?;
-        let mut reset_and_clock_control = peripherals.RCC.constrain();
+        let mut rcc = peripherals.RCC.constrain();
 
-        let core_peripherals = cortex_m::Peripherals::take()?;
         let mut flash = peripherals.FLASH.constrain();
-        let clocks = reset_and_clock_control.cfgr.freeze(&mut flash.acr);
-        let delay = Delay::new(core_peripherals.SYST, clocks);
+        let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-        let mut gpioe = peripherals.GPIOE.split(&mut reset_and_clock_control.ahb);
+        let mut gpioe = peripherals.GPIOE.split(&mut rcc.ahb);
         let leds = Leds::new(
             gpioe.pe8,
             gpioe.pe9,
@@ -43,17 +42,20 @@ impl Init {
         let adc3 = adc::Adc::adc3(
             peripherals.ADC3,
             &mut peripherals.ADC3_4,
-            &mut reset_and_clock_control.ahb,
+            &mut rcc.ahb,
             adc::CkMode::default(),
             clocks,
         );
         let pe7 = gpioe.pe7.into_analog(&mut gpioe.moder, &mut gpioe.pupdr);
         let distance_sensor = Gp2d12::new(pe7, adc3);
 
+        let tim2 = peripherals.TIM2;
+        let timer = Timer::tim2(tim2, Hertz::new(100), clocks, &mut rcc.apb1);
+
         Some(Self {
-            delay,
             leds,
             distance_sensor,
+            timer,
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,76 @@
 #![no_main]
 
 mod gp2d12;
-mod init;
+mod hardware;
 mod model;
+mod state;
 
-use cortex_m_rt::entry;
-use init::Init;
+use hardware::Hardware;
 use model::{Config, Event, Model};
+
+use cortex_m::{asm, peripheral::NVIC};
+use cortex_m_rt::entry;
 use panic_halt as _;
-use stm32f3_discovery::{leds::Leds, stm32f3xx_hal::prelude::*, switch_hal::OutputSwitch};
+use state::State;
+use stm32f3_discovery::{
+    stm32f3xx_hal::{
+        pac::{interrupt, Interrupt},
+        timer,
+    },
+    switch_hal::OutputSwitch,
+};
 
 #[entry]
 fn main() -> ! {
-    let Init {
-        mut leds,
-        mut delay,
-        mut distance_sensor,
-    } = Init::take().unwrap();
-
-    let config = Config {
-        min_distance: distance_sensor.min_distance(),
-        max_distance: distance_sensor.max_distance(),
-        num_leds: leds.iter_mut().len() as u32,
-    };
-    let mut model = Model::new(config);
+    initialize();
+    State::with(update_distance);
 
     loop {
-        let distance = distance_sensor.read();
-        model.update(Event::DistanceUpdated(distance));
-        view(&mut leds, &model);
-
-        delay.delay_ms(25u16);
+        asm::wfi();
     }
 }
 
-fn view(leds: &mut Leds, model: &Model) {
-    let max_led = model.num_leds_on();
+fn initialize() {
+    let mut hardware = Hardware::take().unwrap();
 
-    for (index, led) in leds.iter_mut().enumerate() {
+    let config = Config {
+        min_distance: hardware.distance_sensor.min_distance(),
+        max_distance: hardware.distance_sensor.max_distance(),
+        num_leds: hardware.leds.iter_mut().len() as u32,
+    };
+
+    let model = Model::new(config);
+
+    State::initialize(hardware, model);
+    State::with(start_timer);
+}
+
+fn start_timer(state: &mut State) {
+    state.hardware.timer.listen(timer::Event::Update);
+
+    unsafe {
+        NVIC::unmask(Interrupt::TIM2);
+    }
+}
+
+#[interrupt]
+fn TIM2() {
+    State::with(|state| {
+        state.hardware.timer.clear_update_interrupt_flag();
+        update_distance(state);
+    });
+}
+
+fn update_distance(state: &mut State) {
+    let distance = state.hardware.distance_sensor.read();
+    state.model.update(Event::DistanceUpdated(distance));
+    update_leds(state)
+}
+
+fn update_leds(state: &mut State) {
+    let max_led = state.model.num_leds_on();
+
+    for (index, led) in state.hardware.leds.iter_mut().enumerate() {
         if (index as u32) < max_led {
             led.on().unwrap();
         } else {
